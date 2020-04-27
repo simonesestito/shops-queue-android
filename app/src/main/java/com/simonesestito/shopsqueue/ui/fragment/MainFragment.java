ec1bf94be99ccf105bcdd12529173e9dd92e86e6
Simone Sestito <simone@simonesestito.com>
2020-04-27 14:10:21 +0200

    Add copyright notice and GPL license

--- app/src/main/java/com/simonesestito/shopsqueue/ui/fragment/MainFragment.java
+++ app/src/main/java/com/simonesestito/shopsqueue/ui/fragment/MainFragment.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2020 Simone Sestito
+ * This file is part of Shops Queue.
+ *
+ * Shops Queue is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shops Queue is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shops Queue.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.simonesestito.shopsqueue.ui.fragment;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +26,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.simonesestito.shopsqueue.R;
-import com.simonesestito.shopsqueue.lifecycle.viewmodel.LoginViewModel;
-import com.simonesestito.shopsqueue.model.dto.input.AuthResponse;
 
 /**
  * Main fragment

221adc09430baf08a12ffcb6039b59719b812b5a
Simone Sestito <simone@simonesestito.com>
2020-04-27 18:20:22 +0200

    Create empty Dagger component

---
+++ app/src/main/java/com/simonesestito/shopsqueue/di/Injector.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Simone Sestito
+ * This file is part of Shops Queue.
+ *
+ * Shops Queue is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shops Queue is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shops Queue.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.simonesestito.shopsqueue.di;
+
+import android.content.Context;
+
+import dagger.BindsInstance;
+import dagger.Component;
+
+@Component
+public interface Injector {
+    @Component.Builder
+    public interface Builder {
+        Injector build();
+
+        @BindsInstance
+        Builder provideContext(Context context);
+    }
+}

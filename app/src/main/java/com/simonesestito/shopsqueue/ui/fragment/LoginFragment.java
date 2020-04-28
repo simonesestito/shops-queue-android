b3646bc6481de78fbf302364930eaa7a47473430
Simone Sestito <simone@simonesestito.com>
2020-04-28 08:38:17 +0200

    Remove example redirection from LoginFragment

--- app/src/main/java/com/simonesestito/shopsqueue/ui/fragment/LoginFragment.java
+++ app/src/main/java/com/simonesestito/shopsqueue/ui/fragment/LoginFragment.java
@@ -19,17 +19,12 @@
 package com.simonesestito.shopsqueue.ui.fragment;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.simonesestito.shopsqueue.databinding.LoginFragmentBinding;
-import com.simonesestito.shopsqueue.lifecycle.viewmodel.LoginViewModel;
-import com.simonesestito.shopsqueue.model.dto.input.AuthResponse;
-import com.simonesestito.shopsqueue.model.dto.input.User;
 
 public class LoginFragment extends AbstractAppFragment<LoginFragmentBinding> {
     @Override
@@ -40,21 +35,6 @@ public class LoginFragment extends AbstractAppFragment<LoginFragmentBinding> {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        // FIXME: Remove this
-        new Handler().postDelayed(() -> new ViewModelProvider(requireActivity())
-                .get(LoginViewModel.class)
-                .setAuthStatus(
-                        new AuthResponse(
-                                null,
-                                new User(
-                                        0,
-                                        null,
-                                        null,
-                                        null,
-                                        "USER",
-                                        null
-                                )
-                        )
-                ), 1500);
+        
     }
 }

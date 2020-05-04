fd3d55da06e1480ce600d924dcdab6aa612a8942
Simone Sestito <simone@simonesestito.com>
2020-05-04 10:55:58 +0200

    New admin layout

--- app/src/main/java/com/simonesestito/shopsqueue/ui/recyclerview/AdminUsersAdapter.java
+++ app/src/main/java/com/simonesestito/shopsqueue/ui/recyclerview/AdminUsersAdapter.java
@@ -19,17 +19,24 @@
 package com.simonesestito.shopsqueue.ui.recyclerview;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.simonesestito.shopsqueue.R;
 import com.simonesestito.shopsqueue.api.dto.User;
 import com.simonesestito.shopsqueue.databinding.AdminUsersItemBinding;
 
+
 public class AdminUsersAdapter extends DiffUtilAdapter<User, AdminUsersAdapter.ViewHolder> {
+    private MenuItemListener menuItemListener;
+
     @NonNull
     @Override
     public AdminUsersAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -45,26 +52,52 @@ public class AdminUsersAdapter extends DiffUtilAdapter<User, AdminUsersAdapter.V
         holder.view.userItemEmail.setText(user.getEmail());
 
         @DrawableRes int roleIcon;
+        @StringRes int roleName;
         switch (user.getRole()) {
             case USER:
                 roleIcon = R.drawable.ic_person_black_24dp;
+                roleName = R.string.role_user;
                 break;
             case ADMIN:
                 roleIcon = R.drawable.ic_work_black_24dp;
+                roleName = R.string.role_admin;
                 break;
             case OWNER:
                 roleIcon = R.drawable.ic_shopping_cart_black_24dp;
+                roleName = R.string.role_owner;
                 break;
             default:
                 throw new IllegalArgumentException("Unknown role: " + user.getRole().toString());
         }
         holder.view.userItemIcon.setImageResource(roleIcon);
+        holder.view.userItemRole.setText(roleName);
+
+        if (menuItemListener == null) {
+            holder.view.userItemMenu.setVisibility(View.GONE);
+        } else {
+            holder.view.userItemMenu.setVisibility(View.VISIBLE);
+            holder.view.userItemMenu.setOnClickListener(menuIcon -> {
+                PopupMenu popupMenu = new PopupMenu(menuIcon.getContext(), menuIcon);
+                popupMenu.inflate(R.menu.admin_users_popup_menu);
+                popupMenu.setOnMenuItemClickListener(menuItem -> {
+                    int index = holder.getAdapterPosition();
+                    User clickedUser = getItemAt(index);
+                    menuItemListener.onClick(menuItem, clickedUser.getId());
+                    return true;
+                });
+                popupMenu.show();
+            });
+        }
+    }
+
+    public void setMenuItemListener(MenuItemListener menuItemListener) {
+        this.menuItemListener = menuItemListener;
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         private final AdminUsersItemBinding view;
 
-        public ViewHolder(AdminUsersItemBinding view) {
+        ViewHolder(AdminUsersItemBinding view) {
             super(view.getRoot());
             this.view = view;
         }

39b94d7fd89ba2949512d2312eca538d24046480
Simone Sestito <simone@simonesestito.com>
2020-05-10 18:17:19 +0200

    Handle FCM messages from server

--- app/src/main/java/com/simonesestito/shopsqueue/FcmReceiverService.java
+++ app/src/main/java/com/simonesestito/shopsqueue/FcmReceiverService.java
@@ -18,16 +18,42 @@
 
 package com.simonesestito.shopsqueue;
 
-import androidx.annotation.NonNull;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.simonesestito.shopsqueue.api.dto.Booking;
 import com.simonesestito.shopsqueue.api.dto.FcmToken;
 import com.simonesestito.shopsqueue.api.service.FcmService;
+import com.simonesestito.shopsqueue.ui.MainActivity;
+
+import java.util.Map;
 
 import javax.inject.Inject;
 
 public class FcmReceiverService extends FirebaseMessagingService {
+    private static final String TAG = "Shops Queue FCM";
+    private static final String KEY_MESSAGE_TYPE = "type";
+    private static final String KEY_MESSAGE_DATA = "data";
+    private static final String MESSAGE_TYPE_ALMOST_SHIFT = "almost-shift";
+    private static final String MESSAGE_TYPE_SHIFT = "shift-now";
+    private static final String MESSAGE_TYPE_BOOKING_CANCELLED = "booking-cancelled";
+    private static final String NOTIFICATION_CHANNEL_BOOKINGS_NOTICE_ID = "bookings-notice";
     @Inject FcmService fcmService;
+    @Inject SharedPreferencesStore sharedPreferencesStore;
 
     @Override
     public void onCreate() {
@@ -38,7 +64,129 @@ public class FcmReceiverService extends FirebaseMessagingService {
     @Override
     public void onNewToken(@NonNull String s) {
         super.onNewToken(s);
-        fcmService.addFcmToken(new FcmToken(s))
-                .onError(Throwable::printStackTrace);
+        fcmService.addFcmToken(new FcmToken(s)).onError(Throwable::printStackTrace);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if (sharedPreferencesStore.getAccessToken() == null) {
+            // Discard all messages when not authenticated
+            // The app requires the user to be logged it to be used
+            // This may have been an error.
+            Log.e(TAG, "Message received while logged out");
+            return;
+        }
+
+        Map<String, String> message = remoteMessage.getData();
+        String messageType = message.get(KEY_MESSAGE_TYPE);
+        String jsonData = message.get(KEY_MESSAGE_DATA);
+
+        NotificationManager notificationManager = ContextCompat
+                .getSystemService(this, NotificationManager.class);
+        if (notificationManager == null)
+            return;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    NOTIFICATION_CHANNEL_BOOKINGS_NOTICE_ID,
+                    getString(R.string.notification_channel_booking_notice),
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        switch (messageType != null ? messageType : "null") {
+            case MESSAGE_TYPE_ALMOST_SHIFT:
+                handleAlmostShift(jsonData);
+                break;
+            case MESSAGE_TYPE_SHIFT:
+                handleShift(jsonData);
+                break;
+            case MESSAGE_TYPE_BOOKING_CANCELLED:
+                handleBookingCancellation(jsonData);
+                break;
+            default:
+                Log.e(TAG, "Received payload with unknown type: " + messageType);
+        }
+    }
+
+    private void handleAlmostShift(String jsonString) {
+        Booking data = jsonToBooking(jsonString);
+        if (data == null) {
+            Log.e(TAG, "Received payload with empty Booking data");
+            return;
+        }
+
+        showNotification(
+                data.getShop().getName(),
+                getString(R.string.notification_almost_turn_message)
+        );
+    }
+
+    private void handleShift(String jsonString) {
+        Booking data = jsonToBooking(jsonString);
+        if (data == null) {
+            Log.e(TAG, "Received payload with empty Booking data");
+            return;
+        }
+
+        showNotification(
+                data.getShop().getName(),
+                getString(R.string.notification_your_turn_message)
+        );
+    }
+
+    private void handleBookingCancellation(String jsonString) {
+        Booking data = jsonToBooking(jsonString);
+        if (data == null) {
+            Log.e(TAG, "Received payload with empty Booking data");
+            return;
+        }
+
+        showNotification(
+                data.getShop().getName(),
+                getString(R.string.notification_booking_cancellation_message)
+        );
+    }
+
+    @Nullable
+    private Booking jsonToBooking(String json) {
+        if (json == null || json.isEmpty())
+            return null;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readerFor(Booking.class).readValue(json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void showNotification(String title, String text) {
+        int randomId = (int) (Math.random() * Integer.MAX_VALUE);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                randomId,
+                new Intent(this, MainActivity.class),
+                PendingIntent.FLAG_ONE_SHOT
+        );
+
+        Notification notification = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_BOOKINGS_NOTICE_ID)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                // TODO Use the real app icon
+                .setSmallIcon(R.drawable.ic_shopping_cart_black_24dp)
+                .build();
+
+        NotificationManager notificationManager = ContextCompat.getSystemService(this, NotificationManager.class);
+        if (notificationManager != null) {
+            notificationManager.notify(randomId, notification);
+        }
     }
 }
